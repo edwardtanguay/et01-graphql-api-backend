0486fa4e2d9c69d11957989d0f72f0df3de8d60e
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 19:39:51 +0200

    base server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
-import { cors } from 'cors';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5547;
@@ -30,6 +30,10 @@ app.use(
 app.use(cors({
 	origin: "*",
 }));
+
+app.get('/', (req, res) => {
+	res.send('graphql api');
+})
 
 app.listen(PORT, () => {
     console.log(`API running at: http://localhost:${PORT}`);
