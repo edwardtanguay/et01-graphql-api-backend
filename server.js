f66e1ecd9f124c2ce557ed58b9184e8642746cac
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 19:34:49 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+import { cors } from 'cors';
+
+const app = express();
+const PORT = 5547;
+
+
+const schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`);
+
+const root = {
+	hello: () => {
+		return 'hello world';
+	}
+};
+
+app.use(
+    '/graphql',
+	graphqlHTTP({
+		schema, 
+		rootValue: root,
+        graphiql: true
+    })
+);
+app.use(cors({
+	origin: "*",
+}));
+
+app.listen(PORT, () => {
+    console.log(`API running at: http://localhost:${PORT}`);
+    console.log(`GraphQL running at: http://localhost:${PORT}/graphql`);
+});
