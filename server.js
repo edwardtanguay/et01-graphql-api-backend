2b1fa093be50e198407ae333c00131b9108e6a9a
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-30 23:43:37 +0200

    before schema load

--- server.js
+++ server.js
@@ -2,30 +2,55 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import cors from 'cors';
+import axios from 'axios';
+import schema from './schmema.graphql';
 
 const app = express();
 const PORT = 5547;
+
+app.use(cors());
 
 const schema = buildSchema(`
 	type Query {
 		hello: String,
 		message: String,
-		books: [String]
+		books: [String],
+		employees: [Employee]
+	}
+
+	type Employee {
+		firstName: String,
+		lastName: String
 	}
 `);
 
 const root = {
-	hello: () => {
-		return 'hello world';
-	},
-	message: () => {
-		return 'the message';
-	},
-	books: () => {
-		return ['More About Linux', 'Bash Shell Scripting']
-	}
+    hello: () => {
+        return 'hello world';
+    },
+    message: () => {
+        return 'the message';
+    },
+    books: () => {
+        return ['More About Linux', 'Bash Shell Scripting'];
+    },
+    employees: async () => {
+        const rawEmployees = (
+            await axios.get(
+                'https://edwardtanguay.netlify.app/share/employees.json'
+            )
+        ).data;
+        const employees = [];
+        rawEmployees.forEach((rawEmployee) => {
+            const employee = {
+                firstName: rawEmployee.firstName,
+                lastName: rawEmployee.lastName
+            };
+			employees.push(employee);
+        });
+        return employees;
+    }
 };
-app.use(cors());
 
 // app.all('/', function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -35,17 +60,16 @@ app.use(cors());
 app.use(express.json());
 app.use(
     '/graphql',
-	graphqlHTTP({
-		schema, 
-		rootValue: root,
+    graphqlHTTP({
+        schema,
+        rootValue: root,
         graphiql: true
     })
 );
 
-
 app.get('/', (req, res) => {
-	res.send('graphql api');
-})
+    res.send('graphql api');
+});
 
 app.listen(PORT, () => {
     console.log(`API running at: http://localhost:${PORT}`);
